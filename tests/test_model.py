a457910bc54eb5f1d5719adfdb4d41b81146e9df
georgreen <georgreen.ngunga@andela.com>
2017-09-09 04:36:54 +0300

    Create Models

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,20 @@
-from api.models import User
+from api.models import Activity, Point, Society, User
 from tests.base_test import BaseTestCase
 
 
-class TestUser(BaseTestCase):
-    """Test user model."""
+class ModelsTestCase(BaseTestCase):
+    """Test models."""
 
-    def test_work(self):
-        self.assertTrue(True)
+    def test_models(self):
+        society = Society(
+            color_scheme="#dkneif"
+        )
+
+        user = User(
+            name="dan",
+            email="user@mail.com",
+            role="member",
+            country="Kenya",
+            society=society
+        )
+        self.assertTrue(user.save())
