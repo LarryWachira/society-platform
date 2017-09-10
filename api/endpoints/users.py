e4694ae2cbe07b2b39defae02a3de8712fcc9fa0
Lawrence Wachira <lawrence.wachira@andela.com>
2017-09-10 15:26:12 +0300

    add activity status and times logged to /user/profile
    - add dependencies installation command on circle

--- api/endpoints/users.py
+++ api/endpoints/users.py
@@ -9,15 +9,22 @@ class UserAPI(Resource):
     @token_required
     def get(self):
         _user = g.current_user
-
         user = _user.serialize()
 
         if _user.society:
             user["society"] = _user.society.name
         else:
             user["society"] = None
-        logged_activities = [activity.serialize() for activity in
-                             _user.activities]
+
+        logged_activities = []
+        for _activity in _user.activities:
+            activity = _activity.serialize()
+            activity['status'] = _activity.points.filter_by(
+                activity_id=_activity.uuid, user_id=_user.uuid).first().status
+            logged_activities.append(activity)
+            activity['timesLogged'] = len(_user.activities.filter_by(
+                name=_activity.name).all())
+
         user["loggedActivities"] = logged_activities
         logged_points = sum([point.value for point in _user.points])
         user["loggedPoints"] = logged_points
